7d2f6cec40ac47c9c3c6381d5d0a17ddfa7bf01a
binarystone <87140383+binarystone@users.noreply.github.com>
2022-09-05 07:51:23 +0800

    Create variables.py

---
+++ week2/variables.py
@@ -0,0 +1,13 @@
+# Fill in the blanks to calculate the area of a triangle of base 5, height 3 and output the result.  Reminder:  the area of a triangle is (base*height)/2.
+
+# base = ___
+# height = ___
+# area = ___
+#
+# print(area)
+
+base = 5
+height = 3
+area = (base*height)/2
+
+print(area)
