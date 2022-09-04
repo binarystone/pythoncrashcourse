34773c4213f9cc158b95c6e9e68e9922c40afab5
binarystone <87140383+binarystone@users.noreply.github.com>
2022-09-05 07:49:11 +0800

    Update why-initializating-variables-matter.py

--- week3/why-initializating-variables-matter.py
+++ week3/why-initializating-variables-matter.py
@@ -1,9 +1,18 @@
 # In this code, there's an initialization problem that's causing our function to behave incorrectly. Can you find the problem and fix it?
 
-def count_down(start_number):
+# def count_down(start_number):
+  """
   while (current > 0):
     print(current)
     current -= 1
   print("Zero!")
+  """
+# count_down(3)
 
-count_down(3)
+def count_down(start_number):
+      while (start_number > 0):
+        print(start_number)
+        start_number -= 1
+      print("Zero!")
+    
+    count_down(3)
