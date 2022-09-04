a90884be09bbf5d0c153471764917438d9eba160
binarystone <87140383+binarystone@users.noreply.github.com>
2022-09-05 06:25:51 +0800

    Update module2_numerator-denominator.py

--- module2_numerator-denominator.py
+++ module2_numerator-denominator.py
@@ -1,5 +1,5 @@
 # The fractional_part function divides the numerator by the denominator, and returns just the fractional part (a number between 0 and 1). Complete the body of the function so that it returns the right number.
-Note: Since division by 0 produces an error, if the denominator is 0, the function should return 0 instead of attempting the division.
+# Note: Since division by 0 produces an error, if the denominator is 0, the function should return 0 instead of attempting the division.
 
 def fractional_part(numerator, denominator):
       if denominator == 0 or numerator == 0:
