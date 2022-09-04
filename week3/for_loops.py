7caf09c73a9c0b981c064391f440883499b056ec
binarystone <87140383+binarystone@users.noreply.github.com>
2022-09-05 07:38:55 +0800

    Create for_loops.py

---
+++ week3/for_loops.py
@@ -0,0 +1,12 @@
+# PRACTICE - What is a for loop?
+
+def square(n):
+    return n*n
+
+def sum_squares(x):
+    sum = 0
+    for n in range(x):
+        sum += int(square(n))
+    return sum
+
+print(sum_squares(10)) # Should be 285
