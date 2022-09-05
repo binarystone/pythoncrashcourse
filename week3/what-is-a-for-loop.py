6b7e1e78918de02602cf4d6944828df2f200579f
binarystone <87140383+binarystone@users.noreply.github.com>
2022-09-05 08:42:48 +0800

    Update what-is-a-for-loop.py

--- week3/what-is-a-for-loop.py
+++ week3/what-is-a-for-loop.py
@@ -1,4 +1,5 @@
 # Fill in the gaps of the sum_squares function, so that it returns the sum of all the squares of numbers between 0 and x (not included). Remember that you can use the range(x) function to generate a sequence of numbers from 0 to x (not included).
+
 # def square(n):
 #    return n*n
 #
