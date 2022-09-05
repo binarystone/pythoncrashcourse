c7ceab7ac2c8bb609511b524dafd21e9d93492d4
binarystone <87140383+binarystone@users.noreply.github.com>
2022-09-05 13:59:14 +0800

    Create filenames.py

---
+++ week4/lists-practice-quiz/filenames.py
@@ -0,0 +1,20 @@
+# Given a list of filenames, we want to rename all the files with extension hpp to the extension h. To do this, we would like to generate a new list called newfilenames, consisting of the new filenames. Fill in the blanks in the code using any of the methods you’ve learned thus far, like a for loop or a list comprehension.
+'''
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+___  
+
+print(newfilenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+'''
+
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+newfilenames = []
+for file in filenames:
+  if '.hpp' in file:
+    newfilenames.append((file,file[:-2]))
+  else:
+    newfilenames.append((file,file))
+
+print (newfilenames) # Should be [('program.c', 'program.c'), ('stdio.hpp', 'stdio.h'), ('sample.hpp', 'sample.h'), ('a.out', 'a.out'), ('math.hpp', 'math.h'), ('hpp.out', 'hpp.out')]
