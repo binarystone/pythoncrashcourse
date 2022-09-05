6f0de9090697936b0b09bfc15b2ed84816431aa2
binarystone <87140383+binarystone@users.noreply.github.com>
2022-09-05 12:25:54 +0800

    Update formatting-strings.py

--- week4/formatting-strings.py
+++ week4/formatting-strings.py
@@ -8,7 +8,7 @@
 # print(student_grade("Jesse", 85))
 
 def student_grade(name, grade):
-    return " {} received {}% on the exam".format(name, grade)
+    return "{} received {}% on the exam".format(name, grade)
 
 print(student_grade("Reed", 80))
 print(student_grade("Paige", 92))
