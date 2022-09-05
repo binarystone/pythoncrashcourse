91236a6faa3574c4f7b7508258a1c9104a9aae1b
binarystone <87140383+binarystone@users.noreply.github.com>
2022-09-05 13:57:09 +0800

    Create pig-latin.py

---
+++ week4/lists-practice-quiz/pig-latin.py
@@ -0,0 +1,34 @@
+# The permissions of a file in a Linux system are split into three sets of three permissions: read, write, and execute for the owner, group, and others. Each of the three values can be expressed as an octal number summing each permission, with 4 corresponding to read, 2 to write, and 1 to execute. Or it can be written with a string using the letters r, w, and x or - when the permission is not granted.
+# For example: 
+#  640 is read/write for the owner, read for the group, and no permissions for the others; converted to a string, it would be: "rw-r-----"
+#  755 is read/write/execute for the owner, and read/execute for group and others; converted to a string, it would be: "rwxr-xr-x"
+#  Fill in the blanks to make the code convert a permission in octal format into a string format.
+'''
+def pig_latin(text):
+  say = ""
+  # Separate the text into words
+  words = ___
+  for word in words:
+    # Create the pig latin word and add it to the list
+    ___
+    # Turn the list back into a phrase
+  return ___
+		
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+'''
+
+def pig_latin(text):
+  say = ""
+  # Separate the text into words
+  words = text.split()
+  for word in words:
+    # Create the pig latin word and add it to the list
+    say += word[1:]+word[0]+'ay'
+    if word != words[len(words)-1]:
+      say +=' '
+    # Turn the list back into a phrase
+  return say
+		
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
