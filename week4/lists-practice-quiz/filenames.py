06afc82134afe9299834514cd996409abe56cea1
binarystone <87140383+binarystone@users.noreply.github.com>
2022-09-05 14:03:17 +0800

    Update filenames.py

--- week4/lists-practice-quiz/filenames.py
+++ week4/lists-practice-quiz/filenames.py
@@ -23,3 +23,16 @@ for filename in filenames:
 
 print(newfilenames) 
 # Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+
+# COMPARE WITH
+'''
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+newfilenames = []
+for file in filenames:
+  if '.hpp' in file:
+    newfilenames.append((file,file[:-2]))
+  else:
+    newfilenames.append((file,file))
+
+print (newfilenames) # Should be [('program.c', 'program.c'), ('stdio.hpp', 'stdio.h'), ('sample.hpp', 'sample.h'), ('a.out', 'a.out'), ('m
+'''
