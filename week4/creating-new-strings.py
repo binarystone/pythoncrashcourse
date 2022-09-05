a0314d09f7ccc19bf35a518caa5ff868309e9e6d
binarystone <87140383+binarystone@users.noreply.github.com>
2022-09-05 11:21:02 +0800

    Create creating-new-strings.py

---
+++ week4/creating-new-strings.py
@@ -0,0 +1,7 @@
+# Using the index method, find out the position of "x" in "supercalifragilisticexpialidocious".
+
+# word = "supercalifragilisticexpialidocious"
+# print(___)
+
+word = "supercalifragilisticexpialidocious"
+print(word.index("x"))
