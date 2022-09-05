5e5ce3220af24def462044f5f7da064994ad8bd0
binarystone <87140383+binarystone@users.noreply.github.com>
2022-09-05 10:17:19 +0800

    Create print-numbers.py

---
+++ week3/module3-graded-assessment/print-numbers.py
@@ -0,0 +1,11 @@
+# Fill in the blanks of this code to print out the numbers 1 through 7.
+
+# number = 1
+# while number ___ 7:
+# 	print(number, end=" ")
+# 	___
+
+number = 1
+while number < 8:
+	print(number, end=" ")
+	number = number + 1
